5d39ea9aa3221a8d848948e2f27e5d7c932b0177
Sriuppili <134496793+Sriuppili@users.noreply.github.com>
2026-02-24 15:35:50 +0530

    vpc

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-output "subnet_id_pb" {
+output "public_subnet_id" {
   value = aws_subnet.public.id
 }
-output "subnet_id_pr" {
+output "private_subnet_id" {
   value = aws_subnet.private.id
 }
