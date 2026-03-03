a4c38ed03b5f2963c8fa4356e20f28b542677c04
sriuppili <sriuppili@excelenciaconsulting.com>
2026-03-03 11:16:00 +0530

    updated fargate config

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -87,7 +87,7 @@ resource "aws_ecs_task_definition" "webapp" {
   family                   = "webapp"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "1024"
+  cpu                      = "512"
   memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
